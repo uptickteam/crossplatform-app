69aa3ad50a862a2a2e236338136057c5de9e721a
Nikita <nikita@Alexey-Ustinov-iMac-upt.local>
2017-08-10 17:53:42 +0300

    adding redux

--- android/App.js
+++ android/App.js
@@ -1,68 +1,22 @@
 import React from 'react';
 import { AppRegistry, TouchableHighlight, StyleSheet, Text, View, Platform, TextInput, Button, Alert, ScrollView } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, NavigationActions } from 'react-navigation';
+import ActionButton from 'react-native-action-button';
+import { createStore } from 'redux';
 
-
-const data = [
-  {
-    id: 1,
-    name: 'John Doe',
-    description: 'Some description'
-  },
-  {
-    id: 42,
-    name: 'Jane Doe',
-    description: 'Some descrsdfsdfsdf'
-  }
-];
-
-
-class UserOS extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      message: '',
-    };
-  }
-  render(){ 
-    const msg = Platform.select({
-      ios: {message: 'iOS'},
-      android: {message: 'Android'},
-    });
-    return(
-      <Text>{msg.message}</Text>
-    );
-  }  
-}
 
 class Card extends React.Component{
-
   render(){
     return(
       <TouchableHighlight onPress={this.props.onPress}>
       <View style={this.props.style}>
         <View style={{flexDirection: 'row'}}>
-          <View style={styles.photo} />
-          <View style={styles.text}>
-            <Text>{this.props.name}</Text>
-            <Text>{this.props.description}</Text>
-            {/* <View style={styles.buttons}>
-              <Button
-                title="+"
-                onPress={() => {Alert.alert('Add')}}
-                color="grey"
-              />
-              <Button
-                title="/"
-                onPress={() => {Alert.alert('Edit')}}
-                color="grey"
-              />
-              <Button
-                title="-"
-                onPress={() => {Alert.alert('Delete')}}
-                color="grey"
-              />
-            </View> */}
+          <View style={styles.initials}>
+              <Text>{this.props.initials}</Text>
+          </View>
+          <View style={styles.cardtext}>
+            <Text style={{fontWeight:'bold'}}>{this.props.name}</Text>
+            <Text>{this.props.shortDescription}</Text>
           </View>
         </View>  
       </View>
@@ -71,82 +25,271 @@ class Card extends React.Component{
   }
 }
 
+
+
+
+
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Cards',
   };
 
-  handleCardPress(item) {
-    this.props.navigation.navigate('Card', { item });
+  handleCardPress(styles, item) {
+    this.props.navigation.navigate('Card', { styles, item });
   }
 
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.home}>
-        <View style={{height: 20}} />
         <ScrollView style={styles.scroll}>
           {data.map(item => (
             <Card
               key={item.id}
               style={styles.card}
+              initials={item.initials}
               name={item.name}
-              description='-'
-              onPress={() => this.handleCardPress(item)}
+              shortDescription={item.shortDescription}
+              onPress={() => this.handleCardPress(styles, item)}
             />
           ))}
         </ScrollView>
+        <ActionButton 
+          buttonColor='lightgreen'
+          rotate={0}
+          onPress={() => this.props.navigation.navigate('Editor', {title: 'Add', styles, isEditing:false})}
+        />
       </View>  
     );
   }
 }
 
+
+
+
+
 class CardScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Card Screen'
-  };
+  static navigationOptions = ({navigation}) => ({
+    title: navigation.state.params.item.name,
+    headerRight: <Button title="Edit" onPress={() => navigation.navigate('Editor', {title: 'Edit', styles, isEditing:false})} />
+  });
   render() {
         const { params } = this.props.navigation.state;
-
     return (
-      <View>
-        <Card style={styles.card} name={params.item.name} description={params.description}/>
+      <View style={params.styles.cardscreen}>
+        <View style={params.styles.cardscreentext}>
+          <Text>{params.item.description}</Text>
+        </View>
       </View>
     );
   }
 }
 
-const SimpleApp = StackNavigator({
+
+
+
+
+class EditorScreen extends React.Component {
+  static navigationOptions = ({navigation}) => ({
+    title: navigation.state.params.title,
+    headerRight: 
+      <Button
+        title="OK"
+        onPress={() => {
+          navigation.goBack()
+        }}
+      />
+  });
+  render(){
+    const { params } = this.props.navigation.state;
+    return(
+      <View style={params.styles.editorScreen}>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={params.styles.header}>Name: </Text>
+          <TextInput placeholder={params.isEditing ? params.item.name:"Name"} style={params.styles.input} />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={params.styles.header}>Description: </Text>
+          <TextInput placeholder={params.isEditing ? params.item.shortDescription:"Description"} style={params.styles.input} />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={params.styles.header}>Phone: </Text>
+          <TextInput placeholder={params.isEditing ? params.item.phone:"Phone"} style={params.styles.input} />
+        </View>
+      </View>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+const ScreenNavigator = StackNavigator({
   Home: {screen: HomeScreen},
   Card: {screen: CardScreen},
+  Editor: {screen: EditorScreen},
+  
 });
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   home: {
     flex: 1,
-    height: 100,
     justifyContent: 'space-between',
     marginTop: 0,
   },
+  cardscreen: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    backgroundColor: 'snow',
+  },
+  cardscreenhead: {
+    flex: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    backgroundColor: 'lavender',
+  },
+  editorScreen: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
   card:{
     borderWidth: 1,
+    marginTop: 1,
+    backgroundColor: 'snow'
+  },
+  initials: {
+    flex: 20,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lavender',
+  },
+
+  header: {
+    fontWeight: 'bold',
+    flex: 25,
     marginTop: 5,
   },
-  photo: {
-    flex: 20,
-    height: 75,
-    backgroundColor: 'blue',
-  },
-  text: {
-    backgroundColor: 'skyblue',
-    height: 75,
+  cardtext: {
+    height: 60,
     flex: 80,
+    justifyContent: 'center',
   },
-  buttons: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
+  cardscreentext: {
+    height: 60,
+    flex: 80,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  button: {
+    color: 'red',
+  },
+  input: {
+    backgroundColor: 'snow',
+    flex: 75,
+    marginTop: 5,
   },
 });
 
-export default SimpleApp;
+
+
+
+let store = createStore(cards)
+
+function cards(state = [], action) {
+  switch (action.type) {
+    case 'ADD_CARD':
+      return [
+        ...state,
+        {
+          id: action.id,
+          initials: '',
+          name: action.name,
+          shortDescription: action.description,
+          phone: action.phone,
+          description: '',
+        }
+      ]
+    default:
+      return state
+  }
+}
+
+
+
+const data = [
+  {
+    id: 1,
+    initials: 'JD',
+    name: 'Jack Daw',
+    shortDescription: 'Outcasts',
+    phone: '1',
+    description: '123'+'alfgnaaglajogsm;,h;a,;f;asf'+'asf;agjloajdhag;kaphjapg'+
+    'asgl;jnalgjadjhpdjkshjpdskgp;jsaogdjsz;hbnms;djhodsgh,;'+'algshadisghangloasjgoasglamlgkma',
+  },
+  {
+    id: 2,
+    initials: 'R',
+    name: 'Ramos',
+    shortDescription: 'Arcanists',
+    phone: '2',
+    description: '234',
+  },
+  {
+    id: 3,
+    initials: 'SC',
+    name: 'Sonnia Criid',
+    shortDescription: 'The Guild',
+    phone: '3',
+    description: '345',
+  },
+  {
+    id: 4,
+    initials: 'L',
+    name: 'Lilith',
+    shortDescription: 'Neverborns',
+    phone: '4',
+    description: '456',
+  },
+  {
+    id: 5,
+    initials: 'A',
+    name: 'Asami',
+    shortDescription: 'Ten Thunders',
+    phone: '5',
+    description: '567',
+  },
+  {
+    id: 6,
+    initials: 'N',
+    name: 'Nicodem',
+    shortDescription: 'Ressurectonists',
+    phone: '6',
+    description: '678',
+  },
+  {
+    id: 7,
+    initials: 'O',
+    name: 'Ophelia',
+    shortDescription: 'Gremlins',
+    phone: '7',
+    description: '789',
+  },
+];
+
+export default ScreenNavigator;
